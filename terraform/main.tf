ac2ebce612cd05d879aab4c1a26fe6f82b3f2253
SALLA ABHINAYA <141430666+SALLA-ABHINAYA@users.noreply.github.com>
2025-03-25 22:11:34 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,57 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "backend-rg"
+    storage_account_name = "irmaitfbackendstorage"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
+}
+
 provider "azurerm" {
   features {}
-  version = "~> 3.0"  # Specify the provider  version if necessary
+  subscription_id = "36448a90-905c-4f48-b1b3-deb171f7c247
+"
+  tenant_id       = "38b2a0a6-29a4-4744-ac80-7617428c3bbeD"
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "backend-rg"
-  location = "East US" 
-}
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "backend-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "backend-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_network_interface" "nic" {
-  name                = "backend-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "backend-ipconfig"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "backend-vm"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1s"
-  admin_username      = "azureuser"
-
-  network_interface_ids = [azurerm_network_interface.nic.id]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
+resource "azurerm_resource_group" "example" {
+  name     = "backend-resources"
+  location = "US East"
 }
