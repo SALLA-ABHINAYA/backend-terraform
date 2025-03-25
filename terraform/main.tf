37aee4af3956e5d7516882ae980e7601b3a2532c
SALLA ABHINAYA <141430666+SALLA-ABHINAYA@users.noreply.github.com>
2025-03-25 23:01:00 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,8 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-
-  backend "azurerm" {
-    resource_group_name  = "backend-rg"
-    storage_account_name = "irmaitfbackendstorage"
-    container_name       = "terraform-state"
-    key                  = "terraform.tfstate"
-  }
-}
-
 provider "azurerm" {
   features {}
-  
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "backend-resources"
+  name     = "example-resources"
   location = "US East"
 }
