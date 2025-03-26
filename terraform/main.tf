57718bd664d63125288ea5be26d7009c2496115c
SALLA ABHINAYA <141430666+SALLA-ABHINAYA@users.noreply.github.com>
2025-03-26 14:43:32 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,25 @@
 provider "azurerm" {
   features {}
-  subscription_id = "36448a90-905c-4f48-b1b3-deb171f7c247"
+  subscription_id = "36448a90-905c-4f48-b1b3-deb171f7c247" # Ensure this subscription ID is correct
 }
 
-# Resource Group
-resource "azurerm_resource_group" "irmai_rg" {
-  name     = "14185-irmai-1-jg5p49"
-  location = "Canada Central"
+# Use an existing Resource Group
+data "azurerm_resource_group" "irmai_rg" {
+  name = "14185-irmai-1-jg5p49" # Ensure this name matches the existing resource group
 }
 
 # Virtual Network
 resource "azurerm_virtual_network" "irmai_vnet" {
   name                = "irmai-1-jg5p49-vn"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.irmai_rg.location
-  resource_group_name = azurerm_resource_group.irmai_rg.name
+  location            = data.azurerm_resource_group.irmai_rg.location
+  resource_group_name = data.azurerm_resource_group.irmai_rg.name
 }
 
 # Subnet
 resource "azurerm_subnet" "irmai_subnet" {
   name                 = "irmai-1-jg5p49-sn"
-  resource_group_name  = azurerm_resource_group.irmai_rg.name
+  resource_group_name  = data.azurerm_resource_group.irmai_rg.name
   virtual_network_name = azurerm_virtual_network.irmai_vnet.name
   address_prefixes     = ["10.0.0.0/24"]
 }
@@ -28,8 +27,8 @@ resource "azurerm_subnet" "irmai_subnet" {
 # Network Security Group (NSG) - Best practice default deny inbound
 resource "azurerm_network_security_group" "irmai_nsg" {
   name                = "irmai-default-nsg"
-  location            = azurerm_resource_group.irmai_rg.location
-  resource_group_name = azurerm_resource_group.irmai_rg.name
+  location            = data.azurerm_resource_group.irmai_rg.location
+  resource_group_name = data.azurerm_resource_group.irmai_rg.name
 
   security_rule {
     name                       = "Allow-HTTPS-Out"
@@ -65,8 +64,8 @@ resource "azurerm_subnet_network_security_group_association" "irmai_subnet_nsg_a
 # AKS Cluster
 resource "azurerm_kubernetes_cluster" "irmai_aks" {
   name                = "irmai-1-jg5p49"
-  location            = azurerm_resource_group.irmai_rg.location
-  resource_group_name = azurerm_resource_group.irmai_rg.name
+  location            = data.azurerm_resource_group.irmai_rg.location
+  resource_group_name = data.azurerm_resource_group.irmai_rg.name
   dns_prefix          = "irmai"
 
   default_node_pool {
@@ -84,7 +83,6 @@ resource "azurerm_kubernetes_cluster" "irmai_aks" {
     load_balancer_sku  = "standard"
     dns_service_ip     = "10.0.0.10"
     service_cidr       = "10.0.0.0/16"
-    
   }
 }
 
@@ -128,5 +126,5 @@ resource "azurerm_kubernetes_cluster_node_pool" "sysff" {
 # Optional:  Private DNS Zone (Placeholder if you want to manage via TF)
 resource "azurerm_private_dns_zone" "blob_dns" {
   name                = "privatelink.blob.core.windows.net"
-  resource_group_name = azurerm_resource_group.irmai_rg.name
+  resource_group_name = data.azurerm_resource_group.irmai_rg.name
 }
