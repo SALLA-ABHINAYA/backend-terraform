eb8f7063d128df61bde048e3bee74243bda5bd8c
SALLA ABHINAYA <141430666+SALLA-ABHINAYA@users.noreply.github.com>
2025-03-26 20:51:33 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name   = "14185-irmai-1-jg5p49"  # Your existing resource group
     storage_account_name  = "irmaitfstorage"       # Your storage account name
-    container_name        = "terraform-state"      # Your container name
+    container_name        = "terraform-state"      # Your container  name
     key                   = "infra.tfstate"
   }
 }
