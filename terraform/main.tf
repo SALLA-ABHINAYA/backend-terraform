4922689708c1551eaabf674b3d593e2a0354f651
SALLA ABHINAYA <141430666+SALLA-ABHINAYA@users.noreply.github.com>
2025-03-26 22:12:19 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,9 @@ terraform {
     resource_group_name   = "14185-irmai-1-jg5p49"  # Your existing resource group
     storage_account_name  = "irmaitfstorage"       # Your storage account name
     container_name        = "terraform-state"      # Your container  name
-     key                  = "prod.terraform.tfstate"
+    key                   = "infra.tfstate"
+  }
+}
   }
 }
 
