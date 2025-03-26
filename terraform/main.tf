d91ddcece113b14207020386670dee7610a360ae
SALLA ABHINAYA <141430666+SALLA-ABHINAYA@users.noreply.github.com>
2025-03-26 20:56:37 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,11 @@
-
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "14185-irmai-1-jg5p49"  # Your existing resource group
+    storage_account_name  = "irmaitfstorage"       # Your storage account name
+    container_name        = "terraform-state"      # Your container  name
+    key                   = "infra.tfstate"
+  }
+}
 
 provider "azurerm" {
   features {}
