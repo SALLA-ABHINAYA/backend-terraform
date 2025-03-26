ed45201ef61b0a4ea8ba4de201093f15f6521368
SALLA ABHINAYA <141430666+SALLA-ABHINAYA@users.noreply.github.com>
2025-03-26 22:11:32 +0530

    Delete terraform/backend.tf

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "14185-irmai-1-jg5p49"
-    storage_account_name  = "irmaitfstorage"
-    container_name        = "terraform-state"
-    key                   = "infra.tfstate"
-  }
-}
